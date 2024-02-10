c6bd287b9fd537b19d6d91b486531d0cf775e728
Asad Siddiqui <asadsidd22@outlook.com>
2024-02-09 22:14:17 -0800

    toast error handling added

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -3,21 +3,36 @@
     <form id="form" @submit.prevent="onSubmit">
       <div class="form-control">
         <label for="text">Text</label>
-        <input type="text" id="text" placeholder="Enter text..." />
+        <input type="text" id="text" v-model="text" placeholder="Enter text..." />
       </div>
       <div class="form-control">
         <label for="amount"
           >Amount <br />
           (negative - expense, positive - income)</label
         >
-        <input type="number" id="amount" placeholder="Enter amount..." />
+        <input type="number" id="amount" v-model="amount" placeholder="Enter amount..." />
       </div>
       <button class="btn">Add transaction</button>
     </form>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import {useToast} from "vue-toastification";
+
+const toast = useToast();
+
+const text = ref('');
+const amount = ref('');
+
 const onSubmit = () => {
-  console.log('submit')
+  console.log(text.value, amount.value)
+  if(!text.value || amount.value){
+    toast.error("Both fields must be filled");
+    return;
+  }
+
+  text.value = '';
+  amount.value = '';
 }
 </script>
