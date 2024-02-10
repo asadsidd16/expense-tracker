38f49fa7489cc1737076027bbbe025f38c021d31
Asad Siddiqui <asadsidd22@outlook.com>
2024-02-09 21:33:56 -0800

    added functionality for income

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
-    <Header />
-    <div class="container">
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddTransaction />
-    </div>
+  <Header />
+  <div class="container">
+    <Balance :total="total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
+    <TransactionList :transactions="transactions" />
+    <AddTransaction />
+  </div>
 </template>
 
-<script>
+<script setup>
 import Header from "@/components/Header.vue";
 import Balance from "@/components/Balance.vue";
 import IncomeExpenses from "@/components/IncomeExpenses.vue";
 import TransactionList from "@/components/TransactionList.vue";
 import AddTransaction from "@/components/AddTransaction.vue";
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction
-  },
-};
+import { ref, computed } from "vue";
+
+const transactions = ref([
+  { id: 1, text: "Paycheck", amount: 2229.99 },
+  { id: 2, text: "Flower", amount: -19.99 },
+  { id: 3, text: "Book", amount: 9.99 },
+  { id: 4, text: "Food", amount: 19.99 },
+]);
+
+// Get total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+
+// Get income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+// Get expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
 </script>
--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,4 +1,15 @@
 <template>
-    <h4>Your Balance</h4>
-    <h1 id="balance">$0.00</h1>
+  <h4>Your Balance</h4>
+  <h1 id="balance">{{ Math.round(total * 100) / 100 }}</h1>
 </template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
